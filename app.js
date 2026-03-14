1280473c5302d02d77398fccf6d5465e0f09672c
Soorya <soorisoorya666@gmail.com>
2026-03-14 22:46:29 +0530

    Initialize SmartCampus Dashboard component

---
+++ app.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function App(){
+
+return(
+
+<div style={{textAlign:"center",marginTop:"50px"}}>
+
+<h1>SmartCampus Dashboard</h1>
+
+<h3>Attendance</h3>
+<p>92%</p>
+
+<h3>Assignments Due</h3>
+<p>2 Assignments Pending</p>
+
+<h3>Current Semester</h3>
+<p>Semester 8</p>
+
+</div>
+
+);
+
+}
+
+export default App;
